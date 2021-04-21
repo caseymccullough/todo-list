c215d9429ca6a363fcc970fdb62125121d80a8f6
caseymccullough <kcmccullough@gmail.com>
2021-04-21 10:31:13 -0400

    redo with only todoproject

--- settings.sql
+++
@@ -1,4 +0,0 @@
--- settings.sql
-CREATE DATABASE tunr;
-CREATE USER tunruser WITH PASSWORD 'tunr';
-GRANT ALL PRIVILEGES ON DATABASE tunr TO tunruser;
--- tunr_project/tunr/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Artist
-admin.site.register(Artist)
-from .models import Song
-admin.site.register(Song)
